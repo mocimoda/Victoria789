98f342c4991958f43ed7091a868cae350dc75f41
Victoria789club <moci14807@gmail.com>
2025-06-25 13:23:15 +0700

    Create test_example.py
    Signed-off-by: Victoria789club  <moci14807@gmail.com>

---
+++ test_example.py
@@ -0,0 +1,9 @@
+# test_example.py
+def test_always_passes():
+    """This test should always pass."""
+    assert True
+
+def test_addition():
+    """Tests a simple addition."""
+    result = 2 + 2
+    assert result == 4
